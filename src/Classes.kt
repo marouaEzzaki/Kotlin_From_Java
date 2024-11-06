3106d1d0cf786a69df4713d389b7c6dbc73d16ac
Maroua_Ezzaki_Kadmiri <marouaezzaki23@gmail.com>
2024-11-06 20:44:41 +0100

    Add explanation of Kotlin class, constructor, and method overriding with differences from Java

--- src/Classes.kt
+++ src/Classes.kt
@@ -1,80 +1,71 @@
 fun main() {
-    //en Kotlin no hay = new SmartDevice()
-    val  tv = SmartDevice("Samsung Tv", 1000.0)
-    //tv.brand = "Samsung Tv"
-    //tv.price = 1000.0
+    // In Kotlin, we don't need 'new SmartDevice()' like in Java
+    val tv = SmartDevice("Samsung Tv", 1000.0)
 
-    println("${tv.brand} ${tv.price}")// al sobreescribir el toString solo imprime el objeto
+    println("${tv.brand} ${tv.price}") // When overriding toString, it prints the object state
     tv.turnOn()
-    tv.getDeviceSate()
+    tv.getDeviceState()
 
     println()
 
-
-    val  tv2 = SmartDevice("Samsung Tv", 1000.0)
+    val tv2 = SmartDevice("Samsung Tv", 1000.0)
     tv2.turnOn()
 
-    //println(tv == tv2) -> para que == funcione correctamente, la clase debe sobreescribir el método equals()
-    //triple === -> same place in memory
-    //to check whether the content of the two tvs whe have to override the equals and hashcode
-
-    println("Son iguales tv y tv2 == ? ${tv == tv2}") //da true porq hemos sobreescrito equals()
-    println("Son iguales tv y tv2 === (memory location ) ? ${tv === tv2}")//da false porq esta comparando sus refencias de memoria
-
+    // In order to compare objects with ==, we need to override the equals() method
+    // '===' compares memory references (if they point to the same object in memory)
+    println("Are tv and tv2 equal == ? ${tv == tv2}") // Returns true because we override equals()
+    println("Are tv and tv2 === (memory location) ? ${tv === tv2}") // Returns false because it's comparing references in memory
 
     println()
 
-    val  phone = SmartDevice("Apple Iphone")
-    //phone.brand = "Apple Iphone"
+    val phone = SmartDevice("Apple Iphone")
     phone.price = 1200.0
     println("${phone.brand} ${phone.price}")
 
     phone.turnOff()
-    phone.getDeviceSate()
+    phone.getDeviceState()
 
     println()
+
     val laptop = SmartDevice()
     laptop.brand = "Macbook M3 pro"
     laptop.price = 2500.0
 
-    println("${laptop.brand} is $laptop.price")//instead of doing all of this hacemos el override de toString
+    println("${laptop.brand} is $laptop.price") // Instead of doing all of this, we override toString
 }
 
-//blueprint
-                //no hace falta poner la palabra constructor
-class SmartDevice constructor(//todas las propiedades van dentro del constructores ahora
-    //properties -> attributes (var or val)
+// Blueprint class
+// No need to write 'constructor' explicitly
+class SmartDevice constructor( // Properties go inside the constructor now
     var brand: String?,
     var price: Double?,
-    var isSwitchedOn: Boolean = false
-){
-
-    //para crear mas constructores
+    var isSwitchedOn: Boolean = false // Default value for isSwitchedOn is false
+) {
+    // Additional constructors
     constructor() : this("", 0.0)
     constructor(brand: String) : this(brand, 0.0)
 
-
-    //behaviours -> functions / methods
-    fun turnOn () {
-        isSwitchedOn  = true
+    // Behaviour functions (methods)
+    fun turnOn() {
+        isSwitchedOn = true
         println("$brand is switching on ..")
     }
 
-
-    fun turnOff () {
+    fun turnOff() {
         isSwitchedOn = false
-        println("$brand is  switching off ..")
+        println("$brand is switching off ..")
     }
 
-    fun getDeviceSate () {
-        println("$brand is on ? :  $isSwitchedOn")
+    fun getDeviceState() {
+        println("$brand is on? : $isSwitchedOn")
     }
 
-    //igual que en java solo cambia la sintaxis
+    // Overriding toString method
     override fun toString(): String {
         return "SmartDevice(brand=$brand, price=$price, isSwitchedOn=$isSwitchedOn)"
     }
 
+    // Overriding equals to compare objects based on content
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
@@ -88,6 +79,7 @@ class SmartDevice constructor(//todas las propiedades van dentro del constructor
         return true
     }
 
+    // Overriding hashCode to match equals
     override fun hashCode(): Int {
         var result = brand?.hashCode() ?: 0
         result = 31 * result + (price?.hashCode() ?: 0)
