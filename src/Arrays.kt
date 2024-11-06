6a4dfbcc72ed2e800dcb61db97573096925b5ce2
Maroua_Ezzaki_Kadmiri <marouaezzaki23@gmail.com>
2024-11-06 20:30:49 +0100

    Implement array examples: initialization, access, and modifications in Kotlin

--- src/Arrays.kt
+++ src/Arrays.kt
@@ -1,36 +1,34 @@
 fun main() {
 
-    //Works the same in Java, it allows us to store multiple values
+    // Arrays work similarly in Java, allowing us to store multiple values.
 
-    //The syntax changes, there's no []:
-    val names = arrayOf("Yasmina", "Maroua",2) //if you want to prevent other data types
-                                                //you can specify it : arrayOf<Type>()
+    // Syntax changes slightly in Kotlin there is no [] for arrays:
+    val names = arrayOf("Yasmina", "Maroua", 2)
+    // If you want to prevent other data types, you can specify it: arrayOf<Type>()
 
-    println(names.contentToString()) //to print the content of the array
-    println(names[0]) //para imprimir un valor en especifico lo haces con [i] (same as java)
-    names[2] = "Nadia" //igual que en java para reasignar valores
+    println(names.contentToString())  // To print the content of the array
+    println(names[0])  // To print a specific value, use [i] (same as in Java)
+    names[2] = "Nadia"  // Reassign values in the same way as in Java
 
-    println(names.size) //para saber la longitud de nuestro array
+    println(names.size)  // To get the length of out array
 
-    if ("hello" in names){ //para ver si hello se encuenta dentro del array
+    if ("hello" in names) {  // Check if "hello" is in the array
         println("found")
-    }else{
+    } else {
         println("not found")
     }
 
-    //los arrays como en Java, no se pueden expandir o encoger, su tamaño es fijo
+    // Arrays, like in Java, are fixed in size. They can't be resized.
 
-
-    val arrayOfNulls = arrayOfNulls<String>(5) //para crear un array de nulls
-
+    val arrayOfNulls = arrayOfNulls<String>(5)  // Create an array of nulls with a specified size
     println(arrayOfNulls.contentToString())
 
-    val arrayVacio = emptyArray<String>()//crear un array vacio (LENGHT = 0)
-    println(arrayVacio.contentToString())
+    val emptyArray = emptyArray<String>()  // Create an empty array (LENGTH = 0)
+    println(emptyArray.contentToString())
 
-    intArrayOf(2,3,45)//crear un array de ints
+    val intArray = intArrayOf(2, 3, 45)  // Create an array of Ints
+    println(intArray.contentToString())
 
-    arrayOfNulls.fill("*")
+    arrayOfNulls.fill("*")  // Fill arrayOfNulls with a specific value
     println(arrayOfNulls.contentToString())
-
 }
