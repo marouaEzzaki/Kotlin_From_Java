f7641f9322d738245e1fcdae6f14e4b215e3af1d
Maroua_Ezzaki_Kadmiri <marouaezzaki23@gmail.com>
2024-11-06 20:38:44 +0100

    Implement break and continue examples in Kotlin loops with explanations

--- src/BreakContinue.kt
+++ src/BreakContinue.kt
@@ -1,30 +1,26 @@
 fun main() {
 
-    val nums = intArrayOf(1,2,3,4,5,6,7,8)
+    val nums = intArrayOf(1, 2, 3, 4, 5, 6, 7, 8)
 
-    for (n in nums){
-        if (n%2 ==0) {
+    // Iterate through each number in the array
+    for (n in nums) {
+        if (n % 2 == 0) {
             println(n)
-            continue //to skip specific numbers -> skip whatever comes and then go back to the next iteration
+            continue  // Skip the current iteration and move to the next one
         }
 
-        if (n >5){
-            break //exit the loop
-
+        if (n > 5) {
+            break  // Exit the loop if a number greater than 5 is found
         }
-
-
     }
     println("some other code")
 
-    //break and continue are only allowed in loops
+    // Note: 'break' and 'continue' are only allowed in loops
     /*
     nums.forEach {
-        if (it == 2){
-            continue
+        if (it == 2) {
+            continue  // Skips the current iteration
         }
     }
-    
-     */
-
+    */
 }
