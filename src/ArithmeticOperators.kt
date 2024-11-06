5bc26e116b31beeb75fb319f6c2d0669752c5699
Maroua_Ezzaki_Kadmiri <marouaezzaki23@gmail.com>
2024-11-06 20:26:33 +0100

    Implement basic arithmetic and increment operator examples in Kotlin

--- src/ArithmeticOperators.kt
+++ src/ArithmeticOperators.kt
@@ -1,22 +1,22 @@
 import kotlin.math.max
 import kotlin.math.min
+import kotlin.math.PI
 
 fun main() {
 
     val number = 10
     val number2 = 2
 
-    println("El resultado es : ${number + number2}")
+    println("The result is: ${number + number2}")
 
-    println("el numero pi es : ${kotlin.math.PI}")
+    println("The value of Pi is: ${kotlin.math.PI}")  // Using PI explicitly
 
-    println(max(number,number2))
-    println(min(number,number2))
-    println(PI)
+    println(max(number, number2))
+    println(min(number, number2))  
+    println(PI)  // Now you can use PI directly because it's imported
 
     var numberInc = 10
-    println(++numberInc) //11
-    println(numberInc++) //11 (primero imprime luego incrementa)
-    println(numberInc) //12
-
+    println(++numberInc) // 11
+    println(numberInc++) // 11 (first prints, then increments)
+    println(numberInc) // 12
 }
