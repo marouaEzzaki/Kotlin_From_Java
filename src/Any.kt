4504f764a4b51372bdc31532130ac2168ba843e5
Maroua_Ezzaki_Kadmiri <marouaezzaki23@gmail.com>
2024-11-06 20:20:09 +0100

    Add examples for Any type in Kotlin

--- src/Any.kt
+++ src/Any.kt
@@ -1,12 +1,20 @@
 fun main() {
-    //Any, type which all the types come from
-    //you can store any datatype in that variable
+    // Any is a type in Kotlin from which all other types inherit.
+    // It is similar to Object in Java, but it can be used as a type to hold any data type.
 
-    //no es buena practica, pero por si lo ves sabes lo que es 
+    // Not a recommended practice, but if you ever see it, now you know.
 
-    var number : Int = 10
-    var name : String = "Jamila"
+    var number: Int = 10           // Int type variable
+    var name: String = "Jamila"     // String type variable
 
-    var number2 : Any = 10
-    var name2 : Any = "Jamila"
+    var number2: Any = 10           // Any type variable, holding an Int value
+    var name2: Any = "Jamila"       // Any type variable, holding a String value
+
+    // You can also assign other types to variables of type Any
+    var anyValue: Any = 3.14        // Now it's holding a Double
+
+    // Even though it's of type Any, you may need to cast it back to its original type
+    if (anyValue is Double) {
+        println("The value is a Double: $anyValue")
+    }
 }
