6f65e0998613bc5e956fd82a2c18e984db0d5023
Maroua Ezzaki Kadmiri <157808176+marouaEzzaki@users.noreply.github.com>
2024-11-10 16:14:17 +0100

    Added loop examples using ranges, reverse, and step increments

--- src/LoopsRange.kt
+++ src/LoopsRange.kt
@@ -1,15 +1,16 @@
 fun main() {
 
+    // Simple range loop from 1 to 5 (inclusive)
     for (i in 1..5) println(i)
 
     println("reversed")
+    // Loop in reverse from 5 down to 1
     for (i in 5 downTo 1) println(i)
 
     println("step")
+    // Loop with a step of 2, skipping every second number from 1 to 5
     for (i in 1..5 step 2) println(i)
+
     println("step down")
     for (i in 5 downTo 1 step 2) println(i)
-
-
-
 }
