0f3b165fbeea787145443d3b98b36cb830c2002d
Maroua Ezzaki Kadmiri <157808176+marouaEzzaki@users.noreply.github.com>
2024-11-10 16:13:20 +0100

    Simplified string iteration example and added comparison with Java

--- src/LoopsStrings.kt
+++ src/LoopsStrings.kt
@@ -1,8 +1,14 @@
 fun main() {
 
+    // You can iterate over a String in Kotlin directly using a for-each loop:
     val brand = "marouacode"
 
     for (c in brand) println(c)
 
-
+    // This is similar to Java but is more concise and easier to read:
+    /*
+    for (int i = 0; i < brand.length(); i++) {
+        System.out.println(brand.charAt(i));
+    }
+    */
 }
